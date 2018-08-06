d4ef864e668d40b399e2540ba734eb5933b11970
Bailey <bailey.g.weathers@gmail.com>
2018-08-06 15:14:19 -0400

    Complete move header from App to individual component

---
+++ src/components/app/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import App from './App';
+
+const wrapper = shallow(<App />);
+
+it('renders App without crashing - create-react-app test', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders App without crashing - Enzyme smoke test', () => {
+  shallow(<App />);
+});
+
+it('renders create-react-app message', () => {
+  const welcome = <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it('renders Babysitting with Amy message', () => {
+  const welcome = <h1 className="App-title">Babysitting with Amy</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <header className="header">
+          <h1 className="header-title">Babysitting with Amy</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
