cce73206d546fbb4ff36e0757ba18381018cbff7
Bailey <bailey.g.weathers@gmail.com>
2018-08-06 14:54:31 -0400

    Complete test for basic App

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -12,3 +12,15 @@ it('renders App without crashing - create-react-app test', () => {
 it('renders App without crashing - Enzyme smoke test', () => {
   shallow(<App />);
 });
+
+it('renders create-react-app message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it('renders Babysitting with Amy message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1 className="App-title">Babysitting with Amy</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
