5af1c1420f9f2359c16b53e286d5f4012fa83582
Bailey <bailey.g.weathers@gmail.com>
2018-08-06 18:19:42 -0400

    Complete all of payment levels in Payment Component

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -8,7 +8,21 @@ class Payment extends Component {
         <p className="payment-container">
           Amy worked __ hours from start-time to bedtime at $12/hour for a total of $___.
         </p>
-
+        <p className="payment-container">
+          Amy worked __ hours from bedtime to midnight at $8/hour for a total of $___.
+        </p>
+        <p className="payment-container">
+          Amy worked __ hours from midnight to end of the job at $16/hour for a total of $___.
+        </p>
+        <p className="payment-container">
+          Amy worked __ part hour at $12/hour for a total of $___.
+        </p>
+        <p className="payment-container">
+          Amy worked __ part hour at $18/hour for a total of $___.
+        </p>
+        <p className="final-payment-container">
+          Amy should be paid a total of $___.
+        </p>
       </div>
     );
   }
