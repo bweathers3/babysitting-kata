58ac33fdca04e8aaa8e493d9441c7032f8098d84
Bailey <bailey.g.weathers@gmail.com>
2018-08-06 18:09:51 -0400

    Complete text for first period of time(5 to bedtime)

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -5,6 +5,9 @@ class Payment extends Component {
   render() {
     return (
       <div className="Payment">
+        <p className="payment-container">
+          Amy worked __ hours from start-time to bedtime at $12/hour for a total of $___.
+        </p>
 
       </div>
     );
--- src/components/payment/Payment.test.js
+++ src/components/payment/Payment.test.js
@@ -6,3 +6,9 @@ import Payment from './Payment';
 it('renders Payment Container without crashing', () => {
   shallow(<Payment />);
 });
+
+it('renders Amys Before Bed Time Pay Text', () => {
+  const wrapper = shallow(<Payment />);
+  const beforeBedTimePayText = <p className="payment-container">Amy worked __ hours from start-time to bedtime at $12/hour for a total of $___.</p>
+  expect(wrapper.contains(beforeBedTimePayText)).toEqual(true);
+});
