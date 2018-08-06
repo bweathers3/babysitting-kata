10efc9fa327c285428614aa84149e3016774460d
Bailey <bailey.g.weathers@gmail.com>
2018-08-06 17:41:02 -0400

    Complete Amy's Notes for Babysitting in Notes Component

--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -5,7 +5,18 @@ class Notes extends Component {
   render() {
     return (
       <div className="Notes">
-
+        <p className="notes-container">
+          Amy gets paid $12/hour from start-time to bedtime.
+        </p>
+        <p className="notes-container">
+          Amy gets paid $8/hour from bedtime to midnight.
+        </p>
+        <p className="notes-container">
+          Amy gets paid $16/hour from midnight to end of the job.
+        </p>
+        <p className="notes-container">
+          Amy gets paid for full hours (no fractional hours).
+        </p>
       </div>
     );
   }
--- src/components/notes/Notes.test.js
+++ src/components/notes/Notes.test.js
@@ -6,3 +6,27 @@ import Notes from './Notes';
 it('renders without crashing', () => {
   shallow(<Notes />);
 });
+
+it('renders Amy first Payment Note Text', () => {
+  const wrapper = shallow(<Notes />);
+  const firstPaymentNoteText = <p className="notes-container">Amy gets paid $12/hour from start-time to bedtime.</p>
+  expect(wrapper.contains(firstPaymentNoteText)).toEqual(true);
+})
+
+it('renders Amy second Payment Note Text', () => {
+  const wrapper = shallow(<Notes />);
+  const secondPaymentNoteText = <p className="notes-container">Amy gets paid $8/hour from bedtime to midnight.</p>
+  expect(wrapper.contains(secondPaymentNoteText)).toEqual(true);
+});
+
+it('renders Amy third Payment Note Text', () => {
+  const wrapper = shallow(<Notes />);
+  const thirdPaymentNoteText = <p className="notes-container">Amy gets paid $16/hour from midnight to end of the job.</p>
+  expect(wrapper.contains(thirdPaymentNoteText)).toEqual(true);
+});
+
+it('renders Amy final Payment Note Text', () => {
+  const wrapper = shallow(<Notes />);
+  const finalPaymentNoteText = <p className="notes-container">Amy gets paid for full hours (no fractional hours).</p>
+  expect(wrapper.contains(finalPaymentNoteText)).toEqual(true);
+});
