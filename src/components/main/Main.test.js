bfc7581535d4ca27da10c916fd9cb4566ec20ba9
Bailey <bailey.g.weathers@gmail.com>
2018-08-06 17:08:07 -0400

    refactor Main.test.js

--- src/components/main/Main.test.js
+++ src/components/main/Main.test.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Main from './Main';
 
+const wrapper = shallow(<Main />);
+
 it('renders without crashing', () => {
   shallow(<Main />);
 });
 
 it('renders Amys first babysitting rule Text', () => {
-  const wrapper = shallow(<Main />);
   const firstRuleText = <p className="main-container-top">Amy can not start before 5:00 PM.</p>
   expect(wrapper.contains(firstRuleText)).toEqual(true);
 });
 
 it('renders Amys second babysitting rule Text', () => {
-  const wrapper = shallow(<Main />);
   const secondRuleText = <p className="main-container-bottom">Amy can not leave after 4:00 AM.</p>
   expect(wrapper.contains(secondRuleText)).toEqual(true);
 });
