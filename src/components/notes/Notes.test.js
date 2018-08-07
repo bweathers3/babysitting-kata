e354e117ce307fe1be4f2656ef02c6159f1b8869
Bailey <bailey.g.weathers@gmail.com>
2018-08-07 12:18:00 -0400

    Refactor Notes.js and Notes.test.js to use un-ordered list

--- src/components/notes/Notes.test.js
+++ src/components/notes/Notes.test.js
@@ -10,21 +10,21 @@ it('renders without crashing', () => {
 });
 
 it('renders Amy first Payment Note Text', () => {
-  const firstPaymentNoteText = <p className="notes-container">Amy gets paid $12/hour from start-time to bedtime.</p>
+  const firstPaymentNoteText = <li>Amy gets paid $12/hour from start-time to bedtime.</li>
   expect(wrapper.contains(firstPaymentNoteText)).toEqual(true);
 })
 
 it('renders Amy second Payment Note Text', () => {
-  const secondPaymentNoteText = <p className="notes-container">Amy gets paid $8/hour from bedtime to midnight.</p>
+  const secondPaymentNoteText = <li>Amy gets paid $8/hour from bedtime to midnight.</li>
   expect(wrapper.contains(secondPaymentNoteText)).toEqual(true);
 });
 
 it('renders Amy third Payment Note Text', () => {
-  const thirdPaymentNoteText = <p className="notes-container">Amy gets paid $16/hour from midnight to end of the job.</p>
+  const thirdPaymentNoteText = <li>Amy gets paid $16/hour from midnight to end of the job.</li>
   expect(wrapper.contains(thirdPaymentNoteText)).toEqual(true);
 });
 
 it('renders Amy final Payment Note Text', () => {
-  const finalPaymentNoteText = <p className="notes-container">Amy gets paid for full hours (no fractional hours).</p>
+  const finalPaymentNoteText = <li>Amy gets paid for full hours (no fractional hours).</li>
   expect(wrapper.contains(finalPaymentNoteText)).toEqual(true);
 });
