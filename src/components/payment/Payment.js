d7e71013b1912c31ada7642269fb08b5007b178e
Bailey <bailey.g.weathers@gmail.com>
2018-08-10 13:03:25 -0400

    Remove missed consoe.log

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -6,22 +6,22 @@ class Payment extends Component {
     return (
       <div className="Payment">
         <p className="payment-container">
-          Amy worked __ hours from start-time to bedtime at $12/hour for a total of $___.
+          Amy worked {this.props.hoursBeforeBedTime} hours from start-time to bedtime at $12/hour for a total of ${this.props.billForHoursBeforeBedTime}.
         </p>
         <p className="payment-container">
-          Amy worked __ hours from bedtime to midnight at $8/hour for a total of $___.
+          Amy worked {this.props.hoursAfterBedTimeUntilMidnight} hours from bedtime to midnight at $8/hour for a total of ${this.props.billForHoursAfterBedTimeUntilMidnight}.
         </p>
         <p className="payment-container">
-          Amy worked __ hours from midnight to end of the job at $16/hour for a total of $___.
+          Amy worked {this.props.hoursAfterMidnightUntilFinishTime} hours from midnight to end of the job at $16/hour for a total of ${this.props.billForHoursAfterMidnightUntilFinishTime}.
         </p>
         <p className="payment-container">
-          Amy worked __ part hour at $12/hour for a total of $___.
+          Amy worked {this.props.extraHourBeforeMidnight} part hour at $12/hour for a total of ${this.props.billForExtraHourBeforeMidnight}.
         </p>
         <p className="payment-container">
-          Amy worked __ part hour at $18/hour for a total of $___.
+          Amy worked {this.props.extraHourAfterMidnight} part hour at $18/hour for a total of ${this.props.billForExtraHourAfterMidnight}.
         </p>
         <p className="final-payment-container">
-          Amy should be paid a total of $___.
+          Amy should be paid a total of ${this.props.totalInvoice}.
         </p>
       </div>
     );
